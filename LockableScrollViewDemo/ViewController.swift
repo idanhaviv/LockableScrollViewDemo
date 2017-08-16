685ec359ed3e3d25fac0be4a22c802de468d45e2
idan <idan@soluto.com>
2017-08-16 09:01:06 +0300

    Initial Commit

---
+++ LockableScrollViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LockableScrollViewDemo
+//
+//  Created by Idan Haviv on 16/08/2017.
+//  Copyright © 2017 Idan Haviv. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
